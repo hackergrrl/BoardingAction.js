eb11dc972397ca5fc4437f9048e6b12e60ecfd66
Stephen Whitmore <sww@eight45.net>
2017-03-19 18:37:01 -0700

    goofing around

--- game.js
+++ game.js
@@ -1,18 +1,30 @@
 var recs = require('recs')()
 var PIXI = require('pixi.js')
 
-var app = new PIXI.Application(640, 480, {backgroundColor : 0x1099bb})
+var app = new PIXI.Application(640, 480, {backgroundColor : 0x101644})
 document.body.appendChild(app.view)
 
-function GravityWell () {
-  this.power = 1000
-}
+// --- components ---
 
 function Sprite () {
-  var sprite = PIXI.Sprite.fromImage('bunny.png')
+  var sprite = PIXI.Sprite.fromImage('assets/sprites/bunny.png')
   sprite.anchor.set(0.5)
   app.stage.addChild(sprite)
   return sprite
+}
+
+function Starfield () {
+  var sprite = PIXI.extras.TilingSprite.fromImage('assets/sprites/stars.png')
+  sprite.width = app.renderer.width
+  sprite.height = app.renderer.height
+  sprite.tileScale.x = 0.5
+  sprite.tileScale.y = 0.5
+  sprite.anchor.set(0, 0)
+  app.stage.addChild(sprite)
+  return sprite
+}
+
+function Camera () {
 }
 
 function Body () {
@@ -24,39 +36,48 @@ function Body () {
   this.rotVel = 0
 }
 
-var Blackhole = [Body, GravityWell, Sprite]
+// --- systems ---
 
-recs.system([Body], function (d, delta) {
+recs.system('scroll stars', [Starfield], function (e) {
+  e.starfield.x = -app.stage.x
+  e.starfield.y = -app.stage.y
+  e.starfield.tilePosition.x = app.stage.x
+  e.starfield.tilePosition.y = app.stage.y
+})
+
+recs.system('auto-pan camera', [Body, Camera], function (e) {
+  e.body.x += 0.2
+  e.body.y += 0.3
+
+  app.stage.x = -e.body.x
+  app.stage.y = -e.body.y
+})
+
+recs.system('basic physics', [Body], function (d, delta) {
   d.body.x += d.body.xv * delta
   d.body.y += d.body.yv * delta
   d.body.rot += d.body.rotVel * delta
 })
 
-recs.system(Blackhole, [Body, Sprite], function (g, d, delta) {
-  var dx = g.body.x - d.body.x
-  var dy = g.body.y - d.body.y
-  var len = Math.sqrt(dx*dx + dy*dy) + 0.0001
-  var force = g.gravityWell.power / (len * len)
-  dx /= len
-  dy /= len
-  d.body.xv += force * dx * delta
-  d.body.yv += force * dy * delta
-
-  d.body.rotVel = force
+recs.system('sync body<->sprite', [Body, Sprite], function (e) {
+  e.sprite.x = e.body.x
+  e.sprite.y = e.body.y
+  e.sprite.rotation = e.body.rot
 })
 
-recs.entity(Blackhole, function (e) {
-  console.log('blackhole init')
-  e.body.x = 300
-  e.body.y = 300
-  e.sprite.tint = 0xff44ff
+// --- entities ---
+
+recs.entity([Body, Camera], function (e) {
+})
+
+recs.entity([Starfield], function (e) {
 })
 
 for (var i=0; i < 10; i++) {
   recs.entity([Body, Sprite], function (e) {
     e.body.x = 300
     e.body.y = 200 - i * 10
-    e.body.xv = 3
+    e.body.xv = 1
     e.body.yv = 0
 
     e.sprite.scale.x = 0.5 + i * 0.2
@@ -65,18 +86,6 @@ for (var i=0; i < 10; i++) {
     e.body.rotVel = 0.1
   })
 }
-
-recs.system([Body, Sprite], function (e) {
-  e.sprite.x = e.body.x
-  e.sprite.y = e.body.y
-  e.sprite.rotation = e.body.rot
-})
-
-recs.system(Blackhole, function (e) {
-  var c = Math.sin((new Date()).getTime() / 160) * 0.03 + 1
-  e.sprite.scale.x = c
-  e.sprite.scale.y = c
-})
 
 app.ticker.add(function(delta) {
   recs.tick(delta)
