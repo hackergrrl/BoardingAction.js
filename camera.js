809329b7ca0c3594173bfc7dd64b13d3f8b9caa3
Stephen Whitmore <sww@eight45.net>
2017-03-20 13:36:40 -0700

    basic player controls

--- camera.js
+++ camera.js
@@ -4,10 +4,7 @@ function Camera () {
 }
 
 Camera.install = function (recs, app) {
-  recs.system('auto-pan camera', [Physics, Camera], function (e) {
-    e.physics.x += 0.2
-    e.physics.y += 0.3
-
+  recs.system('sync camera<->stage', [Physics, Camera], function (e) {
     app.stage.x = -e.physics.x
     app.stage.y = -e.physics.y
   })
--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 var recs = require('recs')()
 var PIXI = require('pixi.js')
+var kb = require('kb-controls')
 
 var Camera = require('./camera')
 var Physics = require('./physics')
@@ -11,7 +12,17 @@ var app = new PIXI.Application(640, 480, {backgroundColor : 0x101644})
 document.body.appendChild(app.view)
 global.app = app
 
-// --- import systems ---
+// -- controls setup ---
+var ctl = kb({
+  'W': 'forward',
+  'A': 'left',
+  'S': 'backward',
+  'D': 'right'
+  // '<left>': 'strafe_left'
+// , '<mouse 1>': 'fire'
+})
+
+// --- install systems ---
 Camera.install(recs, app)
 Starfield.install(recs, app)
 Physics.install(recs)
@@ -27,24 +38,59 @@ function makeSprite (imagePath) {
 
 // --- components ---
 
+function ShipController () {
+}
+
+function CameraFollow () {
+  this.target = null
+}
+
 // --- systems ---
+
+recs.system('ship player controls', [Physics, ShipController], function (e) {
+  var speed = 0.03
+
+  if (ctl.forward) {
+    e.physics.yv -= speed
+  }
+  if (ctl.backward) {
+    e.physics.yv += speed
+  }
+  if (ctl.right) {
+    e.physics.xv += speed
+  }
+  if (ctl.left) {
+    e.physics.xv -= speed
+  }
+})
+
+recs.system('camera follow', [Camera, Physics, CameraFollow], function (e) {
+  if (e.cameraFollow.target) {
+    e.physics.x = e.cameraFollow.target.physics.x - app.renderer.width/2
+    e.physics.y = e.cameraFollow.target.physics.y - app.renderer.height/2
+  }
+})
 
 // --- entities ---
 
 recs.entity('star bg', [Starfield], function (e) {})
 
-var Ship = [Physics, PixiSprite]
+var Ship = [Physics, PixiSprite, ShipController]
+var player
 recs.entity('player ship', Ship, function (e) {
   e.physics.x = 300
   e.physics.y = 150
   e.physics.xv = 0.5
   e.physics.yv = 0
-  e.physics.rotVel = 0.1
 
   e.pixiSprite = makeSprite('assets/sprites/fighter.png')
+
+  player = e
 })
 
-recs.entity('camera', [Physics, Camera], function (e) {})
+recs.entity('camera', [Physics, Camera, CameraFollow], function (e) {
+  e.cameraFollow.target = player
+})
 
 // --- run game ---
 
